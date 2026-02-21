8457855a3b851be991efe78e595d5d8fbcfa9fcf
halaarajouh-droid <halaarajouh@gmail.com>
2026-02-21 09:36:48 +0300

    Update and rename faten-app/app.py to app.py

--- app.py
+++ app.py
@@ -17,7 +17,4 @@ def index():
         }
 
     return render_template("home.html", student=student)
-
-if __name__ == "__main__":
-    port=int(os.environ.get("PORT",10000))
-    app.run(host="0.0.0.0",port=port,debug=True)
+    
